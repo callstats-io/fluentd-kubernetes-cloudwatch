6c2170cbe71340fe2cefdb24dd0d6e808aa079cd
Aaron Roydhouse <aaron@roydhouse.com>
2017-02-11 13:29:40 -0500

    Add deployment scripts to create Secret, ConfigMap, and DaemonSet

---
+++ Deploy.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -e
+
+#
+# Create Secret, ConfigMap, and DaemonSet for fluentd-cloudwatch
+#
+
+: ${AWS_ACCESS_KEY_ID?"Must define AWS_ACCESS_KEY_ID"}
+: ${AWS_SECRET_ACCESS_KEY?"Must define AWS_SECRET_ACCESS_KEY"}
+: ${AWS_DEFAULT_REGION?"Must define AWS_DEFAULT_REGION"}
+
+: ${APP_NAME:=fluentd-cloudwatch}
+: ${SECRET_NAME:=$APP_NAME}
+: ${CONFIG_NAME:=$APP_NAME}
+: ${ENV_NAME:=system}
+: ${NAMESPACE:=kube-system}
+: ${IMAGE_NAME:=whereisaaron/fluentd-kubernetes-cloudwatch:latest}
+
+#
+# Secret
+#
+
+if [[ "$(kubectl get secret $SECRET_NAME --namespace=$NAMESPACE --output name 2> /dev/null || true)" = "secret/${SECRET_NAME}" ]]; then
+  ACTION=replace
+else
+  ACTION=create
+fi
+
+kubectl $ACTION --namespace $NAMESPACE -f - <<END
+apiVersion: v1
+kind: Secret
+type: kubernetes.io/opaque
+metadata:
+  name: $SECRET_NAME
+  namespace: $NAMESPACE
+  labels:
+    app: $APP_NAME
+    env: $ENV_NAME
+data:
+  AWS_ACCESS_KEY_ID: $(echo -n "${AWS_ACCESS_KEY_ID}" | base64 -w 0)
+  AWS_SECRET_ACCESS_KEY: $(echo -n "${AWS_SECRET_ACCESS_KEY}" | base64 -w 0)
+END
+
+#
+# ConfigMap
+#
+
+kubectl apply -f - <<END
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: $CONFIG_NAME
+  namespace: $NAMESPACE
+  labels:
+    app: $APP_NAME
+    env: $ENV_NAME
+data:
+  AWS_REGION: $AWS_DEFAULT_REGION
+  CW_LOG_GROUP: kubernetes-cluster
+END
+
+#
+# DaemonSet
+#
+
+export APP_NAME ENV_NAME CONFIG_NAME SECRET_NAME NAMESPACE IMAGE_NAME
+envsubst fluentd-cloudwatch-daemonset-template.yaml | kubectl appy -f -
